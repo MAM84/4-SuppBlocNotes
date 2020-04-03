a75ec495b4fbf7fb0ad8ae224569e5bfb72f4b3e
Marion <marionmetais@hotmail.fr>
2020-04-03 16:23:58 +0200

    work

--- script.js
+++ script.js
@@ -6,8 +6,18 @@ function addNote(e){
     const notes = document.createElement("p");
     notes.textContent = inputNotes.value;
     notes.classList.add("notes");
+
+    const btnSupp = document.createElement("button");
+    btnSupp.textContent = "Supprimer";
+    notes.appendChild(btnSupp);
+    
     blocNotes.appendChild(notes);
     e.preventDefault();
+
+    btnSupp.addEventListener("click",function(){
+        blocNotes.removeChild(notes)
+    })
 }
 
 btnAdd.addEventListener("click",addNote)
+
